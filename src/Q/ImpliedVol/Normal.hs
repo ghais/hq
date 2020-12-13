684803b432ab053d799bd96457e5028ac5a32d82
= <=>
2020-12-12 18:18:41 -0800

    Added Cho-Kim-Kwak method to calculate bachelier implied vol

--- src/Q/ImpliedVol/Normal.hs
+++ src/Q/ImpliedVol/Normal.hs
@@ -3,12 +3,19 @@ import           Q.Bachelier
 import           Q.Types
 import           Statistics.Distribution        (cumulative, density)
 import           Statistics.Distribution.Normal (standard)
+import Numeric.IEEE (epsilon, minNormal, maxFinite)
+
+-- | Method to use to calculate the normal implied vol.
+data Method = Jackel     -- ^ Jackel analytical formula approximation.
+            | ChoKimKwak -- ^ J. Choi, K kim, and M. Kwak (2009)
+
+-- | Default method implementation of 'euImpliedVolWith' using Jackel.
+euImpliedVol = euImpliedVolWith Jackel
 
 -- | Calcualte the bachelier option implied vol of a european option.
---
---
-euImpliedVol :: OptionType -> Forward -> Strike -> YearFrac -> Rate -> Premium -> Vol
-euImpliedVol cp (Forward f) (Strike k) (YearFrac t) (Rate r) (Premium p)
+euImpliedVolWith :: Method -> OptionType -> Forward -> Strike -> YearFrac -> Rate -> Premium -> Vol
+
+euImpliedVolWith Jackel cp (Forward f) (Strike k) (YearFrac t) (Rate r) (Premium p)
   -- Case of no time value. We return 0 vol.
   | p == intrinsinc cp f k = Vol $ 0
   -- Case of ATM. Solve directly. (TODO: configurable threshold?)
@@ -43,3 +50,44 @@ euImpliedVol cp (Forward f) (Strike k) (YearFrac t) (Rate r) (Premium p)
         cpi          = fromIntegral $ fromEnum cp --call put indicartor.
         df           = exp $ (-r) * t
         sqrt2Pi      = 2.506628274631000
+
+
+euImpliedVolWith ChoKimKwak cp (Forward f) (Strike k) (YearFrac t) (Rate r) (Premium p) =
+  let df              = exp $ (-r) * t
+      forwardPremium  = p / df
+      straddlePremium = case cp of Call -> 2 * forwardPremium - (f - k)
+                                   Put  -> 2 * forwardPremium + (f - k)
+      nu'             = (f - k) / straddlePremium
+      nu              = max (-1 + epsilon) (min nu' (1 - epsilon))
+      eta             | abs nu < sqrtEpsilon = 1
+                      | otherwise            = nu / (atanh nu)
+      heta            = h eta
+  in Vol $ sqrt (pi / (2 * t)) * straddlePremium * heta
+
+
+
+
+sqrtEpsilon = sqrt epsilon      
+h eta = sqrt(eta) * (num / den) where
+  a0          = 3.994961687345134e-1
+  a1          = 2.100960795068497e+1
+  a2          = 4.980340217855084e+1
+  a3          = 5.988761102690991e+2
+  a4          = 1.848489695437094e+3
+  a5          = 6.106322407867059e+3
+  a6          = 2.493415285349361e+4
+  a7          = 1.266458051348246e+4
+
+  b0          = 1.000000000000000e+0
+  b1          = 4.990534153589422e+1
+  b2          = 3.093573936743112e+1
+  b3          = 1.495105008310999e+3
+  b4          = 1.323614537899738e+3
+  b5          = 1.598919697679745e+4
+  b6          = 2.392008891720782e+4
+  b7          = 3.608817108375034e+3
+  b8          = -2.067719486400926e+2
+  b9          = 1.174240599306013e+1
+
+  num = a0 + eta * (a1 + eta * (a2 + eta * (a3 + eta * (a4 + eta * (a5 + eta * (a6 + eta * a7))))))
+  den = b0 + eta * (b1 + eta * (b2 + eta * (b3 + eta * (b4 + eta * (b5 + eta * (b6 + eta * (b7 + eta * (b8 + eta * b9))))))))
