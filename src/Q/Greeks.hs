b1f5e6fd38863eb248850859375b3ca4ee2ab180
Ghais <ghais.issa@gmail.com>
2021-02-22 07:46:58 -0500

    Adjust order of Greeks interface

--- src/Q/Greeks.hs
+++ src/Q/Greeks.hs
@@ -3,10 +3,13 @@
 {-# LANGUAGE FlexibleContexts #-}
 module Q.Greeks
   (
-      module Q.Types
-    , module Q.Options
-  )
-where
+    module Q.Types
+  , module Q.Options
+  , Bump (..)
+  , DiffMethod(..)
+  , Bumpable(..)
+  , firstOrder
+  ) where
 
 import Q.Types
 import Q.Options
@@ -21,37 +24,37 @@ data DiffMethod = ForwardDiff
                 | CenteralDiff
 
 class Bumpable a where
-  bumpUp   :: Bump -> a -> a
-  bumpDown :: Bump -> a -> a
-  stepSize :: Bump -> a -> Double
+  bumpUp   :: a -> Bump -> a
+  bumpDown :: a -> Bump -> a
+  stepSize :: a -> Bump -> Double
 
 -- | Things we can bump to calculate Greeks such as 'Spot', 'Rate'..etc'
 instance (Coercible a Double) => Bumpable a where
-  bumpUp (Abs bump) a = coerce $ coerce a + bump
-  bumpUp (Rel bump) a = coerce $ coerce a * (1 + bump)
+  bumpUp a (Abs bump) = coerce $ coerce a + bump
+  bumpUp a (Rel bump) = coerce $ coerce a * (1 + bump)
 
-  bumpDown (Abs bump) a = coerce $ coerce a - bump
-  bumpDown (Rel bump) a = coerce $ coerce a * (1 - bump)
+  bumpDown a (Abs bump) = coerce $ coerce a - bump
+  bumpDown a (Rel bump) = coerce $ coerce a * (1 - bump)
 
-  stepSize (Abs bump) _ = bump
-  stepSize (Rel bump) s = coerce s * bump
+  stepSize _ (Abs bump)  = bump
+  stepSize s (Rel bump) = coerce s * bump
 
 
 
 firstOrder :: (Bumpable a) => DiffMethod -> Bump -> (a -> Double) -> a -> Double
 firstOrder ForwardDiff b f a =  df / dx
   where df = f a' - f a
-        a' = bumpUp b a
-        dx = stepSize b a :: Double
+        a' = bumpUp a b
+        dx = stepSize a b :: Double
 
 firstOrder BackwardDiff d f a = df / dx
    where df = f a - f a'
-         a' = bumpDown d a
-         dx = negate (stepSize d a)
+         a' = bumpDown a d
+         dx = negate (stepSize a d )
 
 firstOrder CenteralDiff b f a = df / dx
    where df = f u - f d
-         u = bumpUp b a
-         d = bumpDown b a
-         dx = 2 * stepSize b a
+         u = bumpUp a b
+         d = bumpDown a b
+         dx = 2 * stepSize a b
 
