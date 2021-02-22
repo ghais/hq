bd943ff963e760e4e1a8b21a7a8ea454b44777b4
Ghais <ghais.issa@gmail.com>
2021-02-22 07:48:51 -0500

    Improve sorted vector to allow it to work with types which dont implement the Numeric.LinearAlgebra Element class

--- src/Q/SortedVector.hs
+++ src/Q/SortedVector.hs
@@ -9,15 +9,16 @@ module Q.SortedVector
   , maxElement
   ) where
 
+import qualified Data.Vector.Algorithms.Merge  as V (sort)
+
 import           Data.Vector.Storable  (Storable)
-import qualified Data.Vector.Storable as V (Vector (..), fromList, length, head, last)
-import qualified Numeric.LinearAlgebra as V (sortVector)
+import qualified Data.Vector.Storable as V (Vector (..), fromList, length, head, last, modify)
 import           Q.Types
 
 newtype SortedVector a = SortedVector (V.Vector a)
 
-fromList as = SortedVector (V.sortVector $ V.fromList as)
-fromVector v = SortedVector (V.sortVector v)
+fromList as = SortedVector (V.modify V.sort $ V.fromList as)
+fromVector v = SortedVector (V.modify V.sort v)
 fromSortedList xs = SortedVector $ V.fromList xs
 
 
